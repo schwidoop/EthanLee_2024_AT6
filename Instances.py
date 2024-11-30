efa730abc15d227a94cf0fc365585f30666c46ca
Ethan Lee De'Buell <eleedeb@gmail.com>
2024-11-30 18:04:44 +0100

    Commit 3

--- Instances.py
+++ Instances.py
@@ -2,3 +2,9 @@ from UserInputValidator import UserInputValidator
 
 validator1 = UserInputValidator()
 validator2 = UserInputValidator()
+
+list1 = ['1', '-3', '-0', 'None', 'Cheese', '42', '5783295', '0', '1.15']
+list2 = ['5.32', '4', '76', 'Software Dev', 'Computer', 'password', '14']
+
+print(validator1.validate_positive_integers(list1))
+print(validator2.validate_positive_integers(list2))
